db659eb7342dc9fa435b967d8e0f0c669d1c9065
Lawson Kurtz <lawson.kurtz@viget.com>
2015-01-12 14:40:43 -0700

    Use faster concatenation operator for concat vs interp test

--- tests/single_concat_vs_double_interp.rb
+++ tests/single_concat_vs_double_interp.rb
@@ -4,18 +4,16 @@ test = Test.new("Single vs Double Quoted Strings (Interpolation vs Concat)", run
 
 test.add_variant "Single Quoted" do
   1000000.times do
-    world = 'world'
-    'Hello' + world
+    'Hello' << 'world'
   end
 end
 
 test.add_variant "Double Quoted" do
   1000000.times do
-    world = "world"
-   "Hello #{world}"
+   "Hello #{"world"}"
   end
 end
 
 test.report
 
-# Results: https://s3.amazonaws.com/kurtzkloud.com/ss/Screen_Shot_2015-01-12_at_10.54.57.png
+# Results: https://s3.amazonaws.com/kurtzkloud.com/ss/Screen_Shot_2015-01-12_at_14.40.12.png
